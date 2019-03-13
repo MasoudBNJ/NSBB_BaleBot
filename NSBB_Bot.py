eb2ed7708ca032588fb955463fdff5e35c9ed851
MasoudMaleki <MalekiBN@gmail.com>
2019-03-13 17:01:29 +0330

    feat: Add cool echo message handler

--- NSBB_Bot.py
+++ NSBB_Bot.py
@@ -3,7 +3,7 @@
 
 import logging
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
 import BotConfigs
@@ -25,6 +25,12 @@ def help(bot, update):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Need Help?!')
 
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text + " to yourself :D")
+
+
 def error(bot, update):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, update.message)
@@ -44,6 +50,9 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
