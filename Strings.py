0404418c4254d634a286488332433a0c0e714fe8
mohsen <emjihash@elenoon.ir>
2019-03-14 13:17:28 +0330

    feat: Make strings cooler

--- Strings.py
+++ Strings.py
@@ -6,25 +6,25 @@ money_request_pan = BotConfigs.receiver_pan
 receipt_success_message = "با موفقیت پرداخت شد.\n شماره پیگیری: {}"
 template_message_title = "یکی از دکمه‌ها رو بفشارید :)"
 template_message_buttons = ['ببر خیزان', 'اجدهای پنهان', 'پیرمرد مهربان']
-reserve_place_title = "نوبت دهی"
-reserve_place_buttons = ['نوبت می خوام']
-added_to_queue_message = 'شمابه صف اضافه شدید و نفر {}ام صف‌اید. نوبتتون شد خبر میدیم'
-exist_in_queue_message = 'شما هم‌اکنون در صف و در ردیف {}ام هستید'
+reserve_place_title = "سلااااااام... می دونم حالت خییییلی خوب نیست...ولی ناچاری که تو صَفِش بری که بعدا تو کَفِش نری :دی...نوبت میگیری یا جور دیگه مشکلت رو حل می کنی؟؟؟"
+reserve_place_buttons = ['نوبت میخوام']
+added_to_queue_message = 'خب ... اومدی تو صف و نفر  {}امی صف‌ای. به نظرم دم در واینسا شلوغ میشه. به کارت برس نوبتت شد خبرت میکنم'
+exist_in_queue_message = 'معلومه بد تحت فشاری‌ها. الان که نوبت {}ی صفی. یا جور دیگه مشکلت رو حل کن یا ۳ بار بگو *شیش سیخ جیگر سیحی شیشزار*'
 
 # Turn arrived
-turn_arrived_menu_title = 'نوبت شماست. می رید داخل؟'
+turn_arrived_menu_title = 'ایول ... نوبتت شد. میری دیگه؟'
 turn_arrived_menu_buttons = ['آره میرم', 'نه. حل شد!']
 other_user_turn_arrived_title = 'نوبت شما رسیده است. می رید داخل؟'
 other_user_turn_arrived_buttons = ['آره', 'دیگه دیر شد!']
 
 # Go inside
-inside_menu_message = 'خوش آمدید.'
-inside_menu_buttons = ['آمدم بیرون']
+inside_menu_message = 'خوش اومدی... راحت باش. البته حواستم به بیرونیه باشه. درم قفل کن.'
+inside_menu_buttons = ['تموم شد...اومدم بیرون']
 
 give_up_message = 'حیف شد!'
 
 # Finish and quite message
-finish_message = 'خسته نباشید!'
+finish_message = 'خسته نباشی دلاور...خدا قوت قهرمان. امیدوارم این چند دقیقه بهت خوش گذشته باشه ... راستی چند دقیقه شد حالا؟!'
 
 # Waiting state strings
 waiting_menu_buttons = ['حاجی فوریه!!']
