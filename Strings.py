b60c8389adb959d8883b5480f567a0accea41969
mohsen <emjihash@elenoon.ir>
2019-03-14 13:27:29 +0330

    feat: Make strings better

--- Strings.py
+++ Strings.py
@@ -6,32 +6,39 @@ money_request_pan = BotConfigs.receiver_pan
 receipt_success_message = "با موفقیت پرداخت شد.\n شماره پیگیری: {}"
 template_message_title = "یکی از دکمه‌ها رو بفشارید :)"
 template_message_buttons = ['ببر خیزان', 'اجدهای پنهان', 'پیرمرد مهربان']
-reserve_place_title = "سلااااااام... می دونم حالت خییییلی خوب نیست...ولی ناچاری که تو صَفِش بری که بعدا تو کَفِش نری :دی...نوبت میگیری یا جور دیگه مشکلت رو حل می کنی؟؟؟"
-reserve_place_buttons = ['نوبت میخوام']
-added_to_queue_message = 'خب ... اومدی تو صف و نفر  {}امی صف‌ای. به نظرم دم در واینسا شلوغ میشه. به کارت برس نوبتت شد خبرت میکنم'
-exist_in_queue_message = 'معلومه بد تحت فشاری‌ها. الان که نوبت {}ی صفی. یا جور دیگه مشکلت رو حل کن یا ۳ بار بگو *شیش سیخ جیگر سیحی شیشزار*'
+
+reserve_place_title = "سلااااااام\nمی‌دونم حالت خییلی خوب نیست... ولی ناچاری که تو صَفِش بری که بعدا تو کَفِش نری " \
+                      ":دی\n...نوبت می‌گیری یا جور دیگه مشکلت رو حل می‌کنی؟؟؟ "
+
+reserve_place_buttons = ['نوبت می‌خوام']
+
+added_to_queue_message = '.خب ... اومدی تو صف و نفر  {}ام صف‌ای. به نظرم دم در وای‌نیسا شلوغ می‌شه. به کارت برس. ' \
+                         'نوبت‌ات شد خبرت می‌کنم '
+
+exist_in_queue_message = 'معلومه بد تحت فشاری‌ها. الان که نوبت {}م صفی. یا جور دیگه مشکلت رو حل کن یا ۳ بار بگو *شیش ' \
+                         'سیخ جیگر سیخی شیشِزار* '
 
 # Turn arrived
-turn_arrived_menu_title = 'ایول ... نوبتت شد. میری دیگه؟'
-turn_arrived_menu_buttons = ['آره میرم', 'نه. حل شد!']
-other_user_turn_arrived_title = 'نوبت شما رسیده است. می رید داخل؟'
+turn_arrived_menu_title = 'ایول ...!\nنوبتت شد. می‌ری دیگه؟'
+turn_arrived_menu_buttons = ['آره می‌رم', 'نه. حل شد!']
+other_user_turn_arrived_title = 'ایول ...!\nنوبتت شد. می‌ری دیگه؟'
 other_user_turn_arrived_buttons = ['آره', 'دیگه دیر شد!']
 
 # Go inside
-inside_menu_message = 'خوش اومدی... راحت باش. البته حواستم به بیرونیه باشه. درم قفل کن.'
-inside_menu_buttons = ['تموم شد...اومدم بیرون']
+inside_menu_message = 'خوش اومدی... راحت باش.\n.البته حواستم به بیرونیه باشه. درم قفل کن.'
+inside_menu_buttons = ['تموم شد... اومدم بیرون']
 
 give_up_message = 'حیف شد!'
 
 # Finish and quite message
-finish_message = 'خسته نباشی دلاور...خدا قوت قهرمان. امیدوارم این چند دقیقه بهت خوش گذشته باشه ... راستی چند دقیقه شد حالا؟!'
+finish_message = 'خسته نباشی دلاور...\nامیدوارم این چند دقیقه بهت خوش گذشته باشه ... راستی چند دقیقه شد حالا؟!'
 
 # Waiting state strings
-waiting_menu_buttons = ['حاجی فوریه!!']
+waiting_menu_buttons = ['حاجی فوریه!!!']
 
 # How important
 how_important_menu_title = 'چقدر فوریه؟؟'
-how_important_menu_buttons = ['یکی جلوتر', 'سه تا جلوتر', 'پنج تا جلوتر', 'همین الان باید برم تو']
+how_important_menu_buttons = ['یکی جلوتر', 'سه تا جلوتر', 'پنج تا جلوتر', 'داره می‌ریزه!']
 how_important_money_request_title = 'خرج داره'
 how_important_money_request_description = 'بالاخره داری تو صف می‌زنی :)'
 
