0fc5173aa6f93025775fe2037b22b9f50ac7d5fe
MasoudMaleki <MalekiBN@gmail.com>
2019-03-14 10:42:15 +0330

    feat: Add reservation flow

--- NSBB_Bot.py
+++ NSBB_Bot.py
@@ -13,12 +13,12 @@ import Strings
 import json
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 users_queue = []
-PLACE_RESERVING_STATE = 0
+PLACE_RESERVING_STATE, TURN_ARRIVED_STATE, INSIDE_STATE = range(3)
 
 
 # Define a few command handlers. These usually take the two arguments bot and
@@ -61,6 +61,11 @@ def send_template_message(bot, update, message, buttons):
                               reply_markup=ReplyKeyboardMarkup(keyboard=buttons))
 
 
+def send_template_message_to_another_user(bot, user_id, message, buttons):
+    bot.send_message(user_id, message,
+                     reply_markup=ReplyKeyboardMarkup(keyboard=buttons))
+
+
 def show_main_menu(bot, update):
     send_template_message(bot=bot,
                           update=update,
@@ -71,13 +76,65 @@ def show_main_menu(bot, update):
 
 def reserve_place(bot, update):
     user_id = update.effective_user.id
-    if user_id in users_queue:
+    if len(users_queue) == 0:
+        users_queue.append(user_id)
+        on_user_turn_arrived(bot, update)
+        return TURN_ARRIVED_STATE
+    elif user_id in users_queue:
         message = Strings.exist_in_queue_message.format(users_queue.index(user_id) + 1)
     else:
         users_queue.append(user_id)
         message = Strings.added_to_queue_message.format(len(users_queue))
 
     update.message.reply_text(message)
+    return TURN_ARRIVED_STATE
+
+
+def on_user_turn_arrived(bot, update):
+    send_template_message(bot=bot,
+                          update=update,
+                          message=Strings.turn_arrived_menu_title,
+                          buttons=[Strings.turn_arrived_menu_buttons])
+    return TURN_ARRIVED_STATE
+
+
+def on_other_user_turn_arrived(bot, user_id):
+    send_template_message_to_another_user(bot,
+                                          user_id,
+                                          message=Strings.turn_arrived_menu_title,
+                                          buttons=[Strings.turn_arrived_menu_buttons])
+    return TURN_ARRIVED_STATE
+
+
+def show_go_inside_menu(bot, update):
+    send_template_message(bot=bot,
+                          update=update,
+                          message=Strings.inside_menu_message,
+                          buttons=[Strings.inside_menu_buttons])
+    return INSIDE_STATE
+
+
+def show_finish_message(bot, update):
+    user_id = update.effective_user.id
+    if user_id in users_queue:
+        users_queue.remove(user_id)
+    update.message.reply_text(Strings.finish_message)
+    notify_other_user(bot, update)
+    return show_main_menu(bot, update)
+
+
+def show_giveup_message(bot, update):
+    user_id = update.effective_user.id
+    if user_id in users_queue:
+        users_queue.remove(user_id)
+    update.message.reply_text(Strings.give_up_message)
+    notify_other_user(bot, update)
+    return show_main_menu(bot, update)
+
+
+def notify_other_user(bot, update):
+    if len(users_queue) > 0:
+        on_other_user_turn_arrived(bot, users_queue[len(users_queue) - 1])
 
 
 def main():
@@ -96,7 +153,13 @@ def main():
 
         states={
             PLACE_RESERVING_STATE: [RegexHandler(pattern='^(' + Strings.reserve_place_buttons[0] + ')$',
-                                                 callback=reserve_place)]
+                                                 callback=reserve_place)],
+            TURN_ARRIVED_STATE: [RegexHandler(pattern='^(' + Strings.turn_arrived_menu_buttons[0] + ')$',
+                                              callback=show_go_inside_menu),
+                                 RegexHandler(pattern='^(' + Strings.turn_arrived_menu_buttons[1] + ')$',
+                                              callback=show_giveup_message)],
+            INSIDE_STATE: [RegexHandler(pattern='^(' + Strings.inside_menu_buttons[0] + ')$',
+                                        callback=show_finish_message)]
         },
 
         fallbacks=[]
--- Strings.py
+++ Strings.py
@@ -11,3 +11,17 @@ reserve_place_buttons = ['نوبت می خوام']
 added_to_queue_message = 'شمابه صف اضافه شدید و نفر {}ام صف‌اید. نوبتتون شد خبر میدیم'
 exist_in_queue_message = 'شما هم‌اکنون در صف و در ردیف {}ام هستید'
 
+# Turn arrived
+turn_arrived_menu_title = 'نوبت شماست. می رید داخل؟'
+turn_arrived_menu_buttons = ['آره میرم', 'نه. حل شد!']
+other_user_turn_arrived_title = 'نوبت شما رسیده است. می رید داخل؟'
+other_user_turn_arrived_buttons = ['آره', 'دیگه دیر شد!']
+
+# Go inside
+inside_menu_message = 'خوش آمدید.'
+inside_menu_buttons = ['آمدم بیرون']
+
+give_up_message = 'حیف شد!'
+
+# Finish and quite message
+finish_message = 'خسته نباشید!'
